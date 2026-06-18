76c12f7d80e2d5b0485d3285788437a7a7d633a0
kentakom1213 <kentakom1213@gmail.com>
2026-06-18 11:19:33 +0900

    refactor: add option for custom separator in password generation

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,41 @@ use clap::Parser;
 use colored::Colorize;
 use rand::seq::IndexedRandom;
 
-const SYMBOLS: &'static str = "!@#$%_-";
+const DEFAULT_SYMBOLS: &'static str = "!@#$%_-";
+const DEFAULT_SEPARATOR: &str = "-";
+const GROUP_SIZE: usize = 4;
 
 /// Generates a password. By default, it uses a combination of uppercase letters, lowercase letters, and digits.
 #[derive(Debug, Parser)]
 #[command(version)]
 struct Args {
-    /// length of password
+    /// Length of password
     #[arg(short = 'n', long, default_value_t = 16)]
     length: usize,
 
-    /// do not use lowercase letters
+    /// Do not use lowercase letters
     #[arg(short = 'l')]
     without_lowercase: bool,
 
-    /// do not use uppercase letters
+    /// Do not use uppercase letters
     #[arg(short = 'u')]
     without_uppercase: bool,
 
-    /// do not use digits
+    /// Do not use digits
     #[arg(short = 'd')]
     without_digits: bool,
 
-    /// use symbols: !@#$%_-
-    #[arg(short = 's')]
-    with_symbols: bool,
-
-    /// use additional characters to the default set
+    /// Use additional characters (If not specified, it defaults to "!@#$%_-")
     #[arg(short = 'a')]
-    additional_characters: Option<String>,
+    additional_characters: Option<Option<String>>,
 
-    /// specify custom character set
+    /// Specify custom character set
     #[arg(short = 'c')]
     custom_characters: Option<String>,
+
+    /// Separate into groups of 4 (If not specified, it defaults to '-')
+    #[arg(short = 'x')]
+    separator: Option<Option<String>>,
 }
 
 fn main() {
@@ -54,12 +56,8 @@ fn main() {
         characters.extend('0'..='9');
     }
 
-    if parser.with_symbols {
-        characters.push_str(SYMBOLS);
-    }
-
     if let Some(additional) = parser.additional_characters {
-        characters.push_str(&additional);
+        characters.push_str(&additional.unwrap_or(DEFAULT_SYMBOLS.to_string()));
     }
 
     // replace characters
@@ -75,7 +73,18 @@ fn main() {
     // choose randomly
     let chosen = choose_ramdomly(&characters, parser.length);
 
-    println!("{chosen}");
+    // separate
+    let output = if let Some(separator) = parser.separator {
+        separate(
+            &chosen,
+            GROUP_SIZE,
+            &separator.unwrap_or(DEFAULT_SEPARATOR.to_string()),
+        )
+    } else {
+        chosen
+    };
+
+    println!("{output}");
 }
 
 /// select n randomly from the given string with duplicates
@@ -85,4 +94,15 @@ fn choose_ramdomly(characters: &str, n: usize) -> String {
     let chars: Vec<char> = characters.chars().collect();
 
     (0..n).filter_map(|_| chars.choose(&mut rng)).collect()
+}
+
+/// separate by separator symbol
+fn separate(input: &str, group_size: usize, separator: &str) -> String {
+    input
+        .chars()
+        .collect::<Vec<_>>()
+        .chunks(group_size)
+        .map(|ch| ch.into_iter().collect::<String>())
+        .collect::<Vec<_>>()
+        .join(separator)
 }
